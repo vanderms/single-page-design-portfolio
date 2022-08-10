6ae7472924df9e0ba0c54305013b09a39eeaa1bd
vanderms <vanderms.84@gmail.com>
2022-08-10 19:16:21 -0300

    feat: implement navbar responsive design

--- functions/app.js
+++ functions/app.js
@@ -3,23 +3,14 @@ const serverless = require('serverless-http');
 const path = require('path')
 const ejs = require('ejs');
 
-
 const app = express();
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  console.log('I am listening');
   res.render('index.ejs');
 });
 
 module.exports.handler = serverless(app);
 
-/*
-
-app.listen(3000, () => {
-  console.log('App running');
-})
-
-*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./views/**/*.{html,ejs}" ],
+  content: [ "./functions/views/**/*.{html,ejs}" ],
   theme: {
+    screens: {
+      "sm": "23.4375rem",
+      "md": "45.125rem",
+      "xl": "71.375rem",
+    },
+    colors: {
+      "neutral": "#FFF",
+      "neutral-100": "#FFF7F0",
+      "neutral-500": "#7A746E",
+      "neutral-800": "#030303",
+      "blue": "#755CDE",
+      "orange": "#F6A560",
+      "pink": "F39E9E",
+      "red": "#EB7565",
+      "cyan": "#61C4B7",
+      "purple": "#552049",
+    },
+
     extend: {},
+
   },
   plugins: [],
 }
